b076eca246d5fc628e3d610eb759ffb04f5dec84
jaco7565 <jaco7565@stud.kea.dk>
2022-03-21 20:35:11 +0100

    Første forsøg på pop_up

---
+++ skolemad/src/components/BestilingOversigt.svelte
@@ -0,0 +1,81 @@
+<script>
+  window.addEventListener("DOMContentLoaded", start);
+
+  function start() {
+    console.log("start");
+
+    document.querySelector(".luk_knap").addEventListener("click", lukPopup);
+    document
+      .querySelector(".open_pop")
+      .addEventListener("click", clickOpenPopupHandler);
+  }
+
+  function lukPopup() {
+    console.log("lukPopup");
+    document.querySelector("#pop_up").style.display = "none";
+  }
+
+  function clickOpenPopupHandler() {
+    console.log("virker det?");
+    document.querySelector("#pop_up").style.display = "block";
+  }
+</script>
+
+<div class="flex">
+  <button class="open_pop"> Bestillings oversigt </button>
+
+  <div id="pop_up">
+    <div class="pop_up_style">
+      <button class="luk_knap">X</button>
+      <h3>Dette er en POP TEST_1</h3>
+    </div>
+  </div>
+</div>
+
+<style>
+  .flex {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+  }
+  .luk_knap {
+    height: 0;
+    width: 0;
+    background-color: none;
+    border: none;
+  }
+  .luk_knap button:hover {
+    background-color: none;
+  }
+  button {
+    height: 4rem;
+    width: 20rem;
+    padding: var(--spacing-2) var(--spacing-4);
+    background-color: var(--background);
+    border: var(--border-width-seconday) solid var(--primary);
+    margin-top: var(--spacing-2);
+    border-radius: var(--border-rounded);
+    cursor: pointer;
+    display: block;
+    transition: ease 250ms;
+    text-align: start;
+    font-weight: 800;
+  }
+
+  button:hover {
+    background-color: var(--font);
+    color: var(--background);
+  }
+  .pop_up_style {
+    border: solid 5px black;
+    width: fit-content;
+    padding: 2rem;
+    margin-top: 2rem;
+  }
+  #pop_up {
+    display: none;
+    position: absolute;
+    top: 37rem;
+    left: 45rem;
+  }
+</style>
--- skolemad/src/pages/Konto.svelte
+++ skolemad/src/pages/Konto.svelte
@@ -1,9 +1,47 @@
 <script>
+  import Router, { location, link } from "svelte-spa-router";
+  import Bestil from "../pages/Bestil.svelte";
+
   import FamilieInfoBox from "../components/FamilieInfoBox.svelte";
   import KontoInfobox from "../components/KontoInfoBox.svelte";
+  import BestillingerValg from "../components/BestillingerValg.svelte";
+  import BestilingOversigt from "../components/BestilingOversigt.svelte";
+  import KontoUdtog from "../components/KontoUdtog.svelte";
 </script>
 
-<h5>jacob was here</h5>
+<div class="flex_1">
+  <FamilieInfoBox />
+  <KontoInfobox />
+</div>
 
-<FamilieInfoBox />
-<KontoInfobox />
+<div class="flex_2">
+  <div class="knapperX4">
+    <BestillingerValg><a href="/#/bestil">Bestil mad</a></BestillingerValg>
+    <BestillingerValg><a href="/#/bestil">Afbestilling</a></BestillingerValg>
+    <BestilingOversigt />
+    <KontoUdtog />
+    <Router
+      routes={{
+        "/bestil": Bestil,
+      }}
+    />
+  </div>
+</div>
+
+<style>
+  .knapperX4 {
+    margin: var(--spacing-8);
+  }
+  .flex_1 {
+    padding-top: var(--spacing-8);
+    display: flex;
+    justify-content: space-between;
+  }
+  .flex_2 {
+    display: flex;
+    justify-content: space-between;
+  }
+  a {
+    text-decoration: none;
+  }
+</style>
