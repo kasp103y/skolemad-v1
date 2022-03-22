1a2eedc18c9ec1666ae05dc2e465d37fe9c7f3c9
Kasper Nikolajsen Dahl <kasp103y@stud.kea.dk>
2022-03-22 14:26:50 +0100

    g

--- skolemad/src/components/Proof.svelte
+++ skolemad/src/components/Proof.svelte
@@ -7,13 +7,13 @@
     border-radius: var(--border-round-full);
     width: 10rem;
     height: 10rem;
-    background-color: var(--white);
-    border: var(--border-width-seconday) solid var(--primary);
+    background-color: var(--knapper);
+    /*  border: var(--border-width-seconday) solid var(--primary); */
     display: flex;
     justify-content: center;
     align-content: center;
     align-items: center;
-    color: var(--font);
-    font-weight: 800;
+    color: var(--white);
+    font-weight: 900;
   }
 </style>
